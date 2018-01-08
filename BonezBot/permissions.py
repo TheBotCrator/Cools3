9d5ea3c57ad2fd844565dee9d27c6e38981a8c16
Spencer Bones <33624631+spbonez@users.noreply.github.com>
2018-01-08 16:33:13 +0100

    added permissions
    added the permissions class from where the permission controller will be placed.

--- BonezBot/permissions.py
+++ BonezBot/permissions.py
@@ -9,7 +9,6 @@ log = logging.getLogger(__name__)
 
 
 class PermissionsDefaults:
-    perms_file = 'config/permissions.ini'
     bot_configs = Config(ConfigDefaults.options_file)
     serverconf_file = 'config/servers.ini'
 
@@ -18,25 +17,48 @@ class PermissionsDefaults:
     GrantToRoles = set()
     UserList = set()
 
-
 class Permissiongroups:
     def __init__(self, config_file):
         self.OwnerID = PermissionsDefaults.bot_configs.owner_id
         self.DevIDs = PermissionsDefaults.bot_configs.dev_ids
         self.serverroles ={}
         self.AdminRoles = []
+        self.BotModRoles = []
+        self.ModRoles = []
         serverconf = configparser.ConfigParser(interpolation=None)
         serverconf.read(PermissionsDefaults.serverconf_file, encoding='utf-8')
 
         for section in serverconf.sections():
-            if not serverconf.get(section, 'Admin_roles'):
+            if not serverconf.get(section, 'Admin_Roles'):
                 print('No Admin Roles set! For: ' + section)
             else:
-                adminroles = serverconf.get(section, 'Admin_roles').split(', ')
+                adminroles = serverconf.get(section, 'Admin_Roles').split(', ')
                 for roleID in adminroles:
                     self.AdminRoles.append(roleID)
-
-
+            
+            if not serverconf.get(section, 'BotMod_Roles'):
+                print('No Bot Mod set! For: ' + section)
+            else:
+                adminroles = serverconf.get(section, 'BotMod_Roles').split(', ')
+                for roleID in adminroles:
+                    self.BotModRoles.append(roleID)
+           
+            if not serverconf.get(section, 'Mod_Roles'):
+                print('No Mod Roles set! For: ' + section)
+            else:
+                adminroles = serverconf.get(section, 'Mod_Roles').split(', ')
+                for roleID in adminroles:
+                    self.ModRoles.append(roleID)
+                    
+class permissions:
+    def __init__(self, configfile):
+        serverconf = configparser.ConfigParser(interpolation=None)
+        serverconf.read(PermissionsDefaults.serverconf_file, encoding='utf-8')
+        self.AdminPerms = serverconf.get('Permissions', 'Admin_Perm').split(', ')
+        self.BotModPerms = serverconf.get('Permissions', 'BotModPerm').split(', ')
+        self.ModPerms = serverconf.get('Permissions', 'Mod_Perm').split(', ')
+        
+        
 class test:
     def test():
         mytest = Permissiongroups(ConfigDefaults.options_file)
